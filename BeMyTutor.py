1908955929ccf92510289ee0c6115a40ef44052e
Adrian <mysliwiec2003@gmail.com>
2024-02-16 17:31:19 -0500

    started code, created mock csv for tutors

--- BeMyTutor.py
+++ BeMyTutor.py
@@ -1,1 +1,11 @@
-#start
+#HackCUNY 2024
+#Be My Tutor
+#Adrian Mysliwiec & Rosa Rivera
+
+import pandas as pd
+import folium
+
+libraries = pd.read_csv("LIBRARY_20240216.csv")     # reads in CSV file with all library locations
+mapNYC = folium.Map(location=[40.75, -74.125], zoom_start=10)       # generates map of NYC
+
+mapNYC.save(outfile = "output.html")        # saves the image as an html
